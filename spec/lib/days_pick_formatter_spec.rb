5a19609e153c977a3bb0f4ff1fbd6647cff146c6
Paul G. Casper <paul.casper@gmail.com>
2021-06-01 01:09:31 -0400

    Initial commit

---
+++ spec/lib/days_pick_formatter_spec.rb
@@ -0,0 +1,11 @@
+require 'days_pick'
+require 'days_pick_formatter'
+
+describe DaysPickFormatter do
+	let(:days_pick) { DaysPick.new(name: 'ADDvantage Tech', symbol: 'AEY', current_price: '2.580') }
+
+	it 'returns proper formatting' do
+		expect(DaysPickFormatter.format(days_pick)).to eq('ADDvantage Tech***AEY***2.580***')	
+	end
+
+end
